b6a2e4bdc15b6dbdace967a18707aaf138fe56cb
Moritz Mueller <moritz.mueller@posteo.de>
2017-06-03 16:27:30 +0200

    commented categories out

--- EMA02_ToDoApp/EditCategoryTableViewCell.swift
+++ EMA02_ToDoApp/EditCategoryTableViewCell.swift
@@ -32,45 +32,49 @@ class EditCategoryTableViewCell: UITableViewCell {
      
      
      */
-    
-    //    func filterForExsistingObject(for cell: EditCategoryTableViewCell){
-    //        if let realm = try? Realm(), let title = self.categoryTitle, let category = realm.object(ofType: Category.self, forPrimaryKey: title) {
-    //
-    //            let exisitingCategorytitle = realm.objects(Category.self).filter("MATCHES %@", category.title)
-    //
-    //            if exisitingCategorytitle.count > 0 {
-    //
-    //                cell.categoryTextfield.text = category.title
-    //
-    //                try! realm.write {
-    //                    category.title = cell.categoryTextfield.text!
-    //                }
-    //
-    //            }else{
-    //                try! realm.write {
-    //                    realm.add(category)
-    //                }
-    //            }
-    //
-    //
-    //        }
-    //    }
-    func filterObjects(with category: Category, for cell: UITableViewCell){
-        if let realm = try? Realm(){
-            let key = category.title
-            let fetchedCategoryObject = realm.object(ofType: Category.self, forPrimaryKey: key)
-            //            let filteredTitles = realm.objects(Category.self).filter("key MATCHES %@", "")
-            if let categoryTitle = fetchedCategoryObject?.title {
-                try! realm.write {
-                    category.title = categoryTitle
-                }
-                let editCell = cell as? EditCategoryTableViewCell
-                editCell?.categoryTextfield.text = category.title
-            }else{
-                try! realm.write {
-                    realm.add(category)
-                }
-            }
-        }
-    }
+    /*
+     ################### Funktions for filtering objects!! Two Versions which are doing the same! Work in Progress!
+     
+     
+     func filterForExsistingObject(for cell: EditCategoryTableViewCell){
+     if let realm = try? Realm(), let title = self.categoryTitle, let category = realm.object(ofType: Category.self, forPrimaryKey: title) {
+     
+     let exisitingCategorytitle = realm.objects(Category.self).filter("MATCHES %@", category.title)
+     
+     if exisitingCategorytitle.count > 0 {
+     
+     cell.categoryTextfield.text = category.title
+     
+     try! realm.write {
+     category.title = cell.categoryTextfield.text!
+     }
+     
+     }else{
+     try! realm.write {
+     realm.add(category)
+     }
+     }
+     
+     
+     }
+     }
+     func filterObjects(with category: Category, for cell: UITableViewCell){
+     if let realm = try? Realm(){
+     let key = category.title
+     let fetchedCategoryObject = realm.object(ofType: Category.self, forPrimaryKey: key)
+     //            let filteredTitles = realm.objects(Category.self).filter("key MATCHES %@", "")
+     if let categoryTitle = fetchedCategoryObject?.title {
+     try! realm.write {
+     category.title = categoryTitle
+     }
+     let editCell = cell as? EditCategoryTableViewCell
+     editCell?.categoryTextfield.text = category.title
+     }else{
+     try! realm.write {
+     realm.add(category)
+     }
+     }
+     }
+     }
+     */
 }
